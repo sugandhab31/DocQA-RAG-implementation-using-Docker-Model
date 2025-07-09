92dcc60ce651ceaa140180ce2a99a86823a60f81
sugandhab31 <sugandhabansal31@gmail.com>
2025-07-09 21:24:46 +0530

    Main.py

---
+++ rag-agent-gemma/app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, UploadFile, File
+from app.doc_parser import parse_document
+from app.rag_engine import query_doc
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to the Gemma3 RAG Agent."
+    }
+
+@app.post("/upload")
+async def upload_document(file:UploadFile = File()):
+    content = await file.read()
+    doc_path = f"data/{file.filename}"
+    with open(doc_path,"wb") as f:
+        f.write(content)
+    return {
+        "message": f"Document {file.filename} uploaded successfully."
+    }
+
+@app.get("/ask/")
+def ask_question(question:str):
+    return query_doc(question)
